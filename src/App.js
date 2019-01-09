1f1074390709f2a3548aba94e8d8123cfddf3a1d
Joshua Hirst <hirst.joshua@me.com>
2019-01-09 15:25:08 +0000

    Basic app setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='app'>
-
+        <div className='header'>
+          <h1>SkyBet</h1>
+        </div>
+        <div className='events'>
+        </div>
       </div>
     );
   }
