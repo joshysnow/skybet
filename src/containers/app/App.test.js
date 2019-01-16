7810003c47f3c3536ec832d5dd5cb8e5d5c7f87c
Joshua Hirst <hirst.joshua@me.com>
2019-01-16 19:47:50 +0000

    Updated test, styled event

--- src/containers/app/App.test.js
+++ src/containers/app/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+
 import App from './App';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = mockStore({
+    app: {
+      events: []
+    },
+    events: {
+      eventDetails: []
+    }
+  });
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
